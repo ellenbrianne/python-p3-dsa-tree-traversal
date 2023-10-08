fcb208634e498e8173b870d9c24c22bdc88243dd
Ellen Bennett <bennette831@gmail.com>
2023-10-08 05:48:13 -0600

    write get_element_by_id method using depth-first tree traversal

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,14 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root = None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        node = self.root
+        nodes_to_visit = [node]
+
+        while len(nodes_to_visit) > 0:
+            node = nodes_to_visit.pop(0)
+            if node['id'] == id:
+                return node
+            else:
+                nodes_to_visit = node['children'] + nodes_to_visit
